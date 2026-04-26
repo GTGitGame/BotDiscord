4dd1d091ba48943bbeb84955643f8138bf1617e3
GTGitGame <giulian.polidori@gmail.com>
2026-04-26 18:51:04 +0200

    Update port for Render

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,10 @@ def home():
 
 def run():
     # Render utilise le port 10000 par défaut
-    app.run(host='0.0.0.0', port=8080)
+    # Render utilise souvent le port 10000, 
+    # mais il est plus sûr d'utiliser la variable d'environnement PORT
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
