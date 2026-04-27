36a64f5b76a1427c7607711b4379f9a8c8e485ed
GTGitGame <giulian.polidori@gmail.com>
2026-04-27 12:51:37 +0200

    Correction des Bugs de communication serveur

--- keep_alive.py
+++ keep_alive.py
@@ -9,12 +9,13 @@ def home():
     return "Le bot est en ligne !"
 
 def run():
-    # Render utilise le port 10000 par défaut
-    # Render utilise souvent le port 10000, 
-    # mais il est plus sûr d'utiliser la variable d'environnement PORT
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    # On ajoute threaded=True pour plus de sécurité
+    app.run(host='0.0.0.0', port=port, threaded=True)
 
 def keep_alive():
+    # On s'assure que le thread est bien lancé en arrière-plan
     t = Thread(target=run)
     t.start()
+    print("Serveur de monitoring lancé !")
+
